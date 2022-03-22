47395ef4fcde10c9eff6834910c8d3a248a18d6e
The Guru Asia - Hive/Steem <88762648+theguruscripts@users.noreply.github.com>
2022-03-22 08:23:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# spk_claim_bot
+# dividend_share_bot
+This script can be used to share dividend for STAKING your token like BROFund, TANGENT & etc in Hive-Engine.
+
+***
+## Installation
+This is a nodejs based application.
+```
+npm install --save
+```
+***
+## Configuration
+```javascript
+{
+	"enabled" : true,					    //Set true to initiate dividend share process	
+	"trigger" : {						    //Set UTC Time to initiate the process (Default 00:15am)		
+		"hour" : 0,
+		"minute" : 15
+	},	
+	"ssc_api" : "https://api.hive-engine.com/rpc",              //Hive-Engine Api Call
+	"json_id" : "ssc-mainnet-hive",                             //Hive-Engine Json Id
+	"staker_count" : 200,                                       //Number of stakers of the token
+	"decimal" : 1000,
+	"timeout" : 5000,
+	"rec_timeout" : 1000,
+	"he_timeout" : 9000,
+	"block_timeout" : 9000,	
+	"contract_setting" : {
+		"token" : "",                                             //Add "Token Symbol" that you want to stake to share dividends
+		"contract" : "tokens",		
+		"table" : "balances",
+		"limit" : 1000
+	},
+	"reward_pools_setting" : {
+		"pools" : [
+			//["TOKEN_NAME", 
+                        // "IF SEND ENTIRE ACCOUNT TOKEN_BALANCE - SET balTrue, ELSE - SET balFalse",
+                        // "PERCENTAGE YOU WANT TO SHARE - (DONT ADD % MARK)",
+                        // "IF balFalse - DEFINE HOW MANY TOKENS YOU WISH TO SHARE",
+                        // "IF mintTrue - IT WILL MINT PREVIOUSLY DEFINED TOKEN AMOUNT",
+                        // "if availFalse - THIS TOKEN WILL NOT SHARE TO REWARD POOL",
+                        // "TOKEN_SENDER_ACCOUNT_NAME",
+                        // "TOKEN_SENDER_ACTIVE_KEY"]
+     	//You can add tokens like below      
+	["TOKEN_NAME_1", "balFalse", "100", "12", "mintFalse", "availTrue", "TOKEN_1_SENDER_NAME", "ACTIVE_KEYS"],
+      	["TOKEN_NAME_2", "balTrue", "75", "", "mintFalse", "availTrue", "TOKEN_2_SENDER_NAME", "ACTIVE_KEYS"],
+      	["TOKEN_NAME_3", "balFalse", "90", "50", "mintTrue", "availTrue", "TOKEN_3_SENDER_NAME", "ACTIVE_KEYS"]
+		],
+		"mint_setting" : {	                                            //If token mints to reward pool share, this will be used.		
+			"contract" : "tokens",
+			"action" : "issue",
+			"event" : "transferFromContract",				
+			"mint_memo" : "Mint {{tokensymbol}} Tokens To Reward Pool Share - ({{date}} / {{time}} Server Time)"
+		},
+		"reward_setting" : {
+			"distributor_account" : "",                                   //Pool Rewards distributor account name
+			"distributor_activekey" : "",                                 //Pool Rewards distributor account active keys 
+			"contract" : "tokens",
+			"action" : "transfer",
+			"event" : "transfer",
+			"reward_memo" : "Congratulations, Your Rank {{stakerrank}} & You Earned {{stakerreward}} {{tokensymbol}} For Staking {{staketokensymbol}}"
+		},
+		"balance_setting" : {
+			"contract" : "tokens",
+			"table" : "balances"
+		}
+	},
+	"db_string" : "",                                                //Mongo Db Atlas - Connection Link
+	"db_name" : "pooldb",
+	"db_table_name" : "pooltb",
+	"db_remove_days" : 7,                                           //Due to use of free cluster (500Mb) better define maximum days to keep pool reward sending data. 
+	"rpc_nodes" : [
+		"https://api.deathwing.me",
+		"https://hive.roelandp.nl",
+		"https://api.openhive.network",
+		"https://rpc.ausbit.dev",
+		"https://hived.emre.sh",
+		"https://hive-api.arcange.eu",
+		"https://api.hive.blog",
+		"https://api.c0ff33a.uk",
+		"https://rpc.ecency.com",
+		"https://anyx.io",
+		"https://techcoderx.com",
+		"https://hived.privex.io",
+		"https://api.followbtcnews.com/"
+	]
+}
+```
+***
+## Execute
+```
+node app.js
+```
+***
+## Development
+Encounter any issue or Bugs, Please report them [Here](https://github.com/theguruscripts/dividend_share_bot/issues).
+
+The Script Developed by @theguruasia on HIVE.BLOG, @TheGuruAsia theguruasia#8947 on Discord.
---
+++ app.js
@@ -0,0 +1,268 @@
+const { Hive } = require('@splinterlands/hive-interface');
+const { HiveEngine } = require('@splinterlands/hive-interface');
+const axios = require('axios');
+const colors = require('colors');
+const config = require('./config.json');
+const schedule = require('node-schedule');
+
+const hive = new Hive({
+	logging_level: 0,
+	rpc_nodes: config.rpc_nodes
+});
+let hive_engine = new HiveEngine();
+
+var SPKAPI = config.spk_api;
+var JSONIDCLAIM = config.json_id_claim;
+var JSONIDSEND = config.json_id_send;
+
+var SENDTO = config.send_to;
+var SENDMEMO = config.send_memo;
+
+var DECIMAL = config.decimal;
+DECIMAL = parseInt(DECIMAL) || 0;
+var TIMEOUT = config.timeout;
+TIMEOUT = parseInt(TIMEOUT) || 0;
+var RECTIMEOUT = config.rec_timeout;
+RECTIMEOUT = parseInt(RECTIMEOUT) || 0;
+var HETIMEOUT = config.he_timeout;
+HETIMEOUT = parseInt(HETIMEOUT) || 0;
+var BLOCKTIMEOUT = config.block_timeout;
+BLOCKTIMEOUT = parseInt(BLOCKTIMEOUT) || 0;
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const processLARYNX = async () => {
+	try 
+	{
+		await claimProcess();
+		await sendProcess();
+	} 
+	catch (error) 
+	{
+		console.log("Error at processLARYNX() : ", error);
+	}
+}
+
+const claimProcess = async () => {
+	try 
+	{
+		var userList = config.user_setting.accounts;
+		if(userList.length > 0)
+		{
+			async function recursive(n)
+			{
+				if (n <= userList.length - 1) 
+				{
+					var accountName = userList[n][0];
+					var accountActiveKey = userList[n][1];
+					var claimStatus = userList[n][2];
+					var sendStatus = userList[n][3];
+
+					if(claimStatus == true)
+					{
+						var alreadyClaimed = await getClaimRequest(accountName);
+						if(alreadyClaimed == false)
+						{
+							var claimStatus = await claimLARYNX(accountName, accountActiveKey);
+							if(claimStatus == true)
+							{
+								console.log(accountName, "TOKEN CLAIMED SUCCESSFULLY".yellow);
+							}
+							else
+							{
+								console.log(accountName, "TOKEN CLAIM FAILED".red);
+							}		
+							await timeout(RECTIMEOUT);
+							await recursive(n + 1);
+						}
+						else
+						{
+							console.log(accountName, "TOKEN ALREADY CLAIMED".green);
+							await timeout(RECTIMEOUT);
+							await recursive(n + 1);
+						}
+					}
+					else
+					{
+						console.log(accountName, "CLAIM STATUS FALSE".red);
+						await timeout(RECTIMEOUT);
+						await recursive(n + 1);
+					}
+				}				
+				else
+				{
+					console.log("ALL USER CLAIMS PROCESSED".blue);
+					
+				}
+			}
+			await recursive(0);
+		}
+	} 
+	catch (error) 
+	{
+		console.log("Error at claimProcess() : ", error);
+	}
+}
+
+const getClaimRequest = async (accountName) => {
+	var claimStatus = false;
+    try 
+	{
+        const res = await axios.get(SPKAPI + accountName);
+		var lastClaim = parseInt(res.data.drop.last_claim) || 0;
+		var totalClaims = parseInt(res.data.drop.total_claims) || 0;
+		
+		if(lastClaim != 0 && totalClaims != 0)
+		{
+			if(lastClaim == totalClaims)
+			{
+				claimStatus = true;
+			}
+		}		
+		return claimStatus;
+    } 
+	catch (error) 
+	{
+		console.log("Error at getClaimRequest() : ", error);
+		return claimStatus;
+    }
+};
+
+const claimLARYNX = async (accountName, accountActiveKey) => {
+	var claimStatus = false;
+	try 
+	{
+		var transferObj = {};
+
+		let claimTrans = await hive.custom_json(JSONIDCLAIM, transferObj, accountName, accountActiveKey, true);
+		await timeout(HETIMEOUT);
+		console.log("claimTrans : ", claimTrans);
+		if(claimTrans.id != "") 
+		{
+			console.log("CLAIM SUCCESSFULLY".blue);			
+			claimStatus = true;			
+		}
+		return claimStatus;
+	} 
+	catch (error) 
+	{
+		console.log("Error at claimLARYNX() : ", error);
+		return claimStatus;
+	}
+}
+
+const sendProcess = async () => {
+	try 
+	{
+		var userList = config.user_setting.accounts;
+		if(userList.length > 0)
+		{
+			async function recursive(n)
+			{
+				if (n <= userList.length - 1) 
+				{
+					var accountName = userList[n][0];
+					var accountActiveKey = userList[n][1];
+					var claimStatus = userList[n][2];
+					var sendStatus = userList[n][3];
+
+					if(sendStatus == true)
+					{
+						var balanceData = await getBalanceRequest(accountName);
+						if(balanceData > 0.0)
+						{
+							var tokenSendStatus = await sendBalance(accountName, accountActiveKey, balanceData);  
+							if(tokenSendStatus == true)
+							{
+								console.log(accountName, "TOKEN SENT SUCCESSFULLY".yellow);
+							}
+							else
+							{
+								console.log(accountName, "TOKEN FAILED TO SENT".yellow);
+							}
+							await timeout(RECTIMEOUT);
+							await recursive(n + 1);
+						}
+						else
+						{
+							console.log(accountName, "NO ENOUGH BALANCE".red);
+							await timeout(RECTIMEOUT);
+							await recursive(n + 1);
+						}
+					}
+					else
+					{
+						console.log(accountName, "SEND STATUS FALSE".red);
+						await timeout(RECTIMEOUT);
+						await recursive(n + 1);
+					}
+				}				
+				else
+				{
+					console.log("TOKEN SENDING PROCESS FINISHED".blue);
+				}
+			}
+			await recursive(0);
+		}
+	} 
+	catch (error) 
+	{
+		console.log("Error at sendProcess() : ", error);
+	}
+}
+
+const getBalanceRequest = async (accountName) => {
+	var tokenBalance = 0.0;
+    try 
+	{
+        const res = await axios.get(SPKAPI + accountName);
+        tokenBalance = parseInt(res.data.balance) || 0;
+		return tokenBalance;
+    } 
+	catch (error) 
+	{
+		console.log("Error at getBalanceRequest() : ", error);
+		return tokenBalance;
+    }
+};
+
+const sendBalance = async (accountName, accountActiveKey, tokenAmount) => {
+	var sendStatus = false;
+	try 
+	{
+		var transferObj = {
+			to: SENDTO,
+			amount: tokenAmount,
+			memo: SENDMEMO,
+			from: accountName
+		};
+
+		let sendTrans = await hive.custom_json(JSONIDSEND, transferObj, accountName, accountActiveKey, true);
+		await timeout(HETIMEOUT);
+		console.log("sendTrans : ", sendTrans);
+		if(sendTrans.id != "") 
+		{
+			console.log("TOKEN SENT SUCCESSFULLY".blue);			
+			sendStatus = true;			
+		}
+		return sendStatus;
+	} 
+	catch (error) 
+	{
+		console.log("Error at sendBalance() : ", error);
+		return sendStatus;
+	}
+}
+
+// trigger at utc time
+const ruleProcess = {
+	...config.trigger,
+	tz: 'Etc/UTC',
+};
+if(config.enabled == true)
+{
+	schedule.scheduleJob(ruleProcess, processLARYNX);
+}
+processLARYNX();
